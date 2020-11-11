23d03ad7e0c299cd4e96cc964774599ad14d9a5d
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-11-11 20:04:19 +0530

    first try

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,23 @@
+import React from 'react';
 // write code for Welcome component here
+// export default class Welcome extends React.Component{
+
+//     render(){
+//         return (
+//             <div>
+//                 <h1>Hey {this.props.name}!</h1>
+//                 <h2>Welcome to Newton School.</h2>
+//             </div>
+//             );
+//     }
+
+// }
+
+//functional component
+export default function Welcome(props){
+    return(<div>
+        <h1>Hey {props.name}!</h1>
+        <h2>Welcome to Newton School.</h2>
+    </div>);
+
+}
