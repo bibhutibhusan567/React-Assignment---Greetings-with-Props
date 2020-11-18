0698066f39ec4ce97bb136a650b58b0a984cf4f0
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-11-18 17:01:25 +0530

    lets try

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,23 +1,18 @@
-import React from 'react';
-// write code for Welcome component here
-// export default class Welcome extends React.Component{
+import React from "react"
 
-//     render(){
-//         return (
-//             <div>
-//                 <h1>Hey {this.props.name}!</h1>
-//                 <h2>Welcome to Newton School.</h2>
-//             </div>
-//             );
+// export default class Welcome extends React.Component {
+//     render() {
+//         return (<div>
+//             <h1>Hey {this.props.name}!</h1>
+//             <h2>Welcome to Newton School.</h2>
+//         </div>);
+
 //     }
-
 // }
 
-//functional component
-export default function Welcome(props){
-    return(<div>
+export default function welcome(props) {
+    return (<div>
         <h1>Hey {props.name}!</h1>
         <h2>Welcome to Newton School.</h2>
     </div>);
-
 }
